0b1a7ac0ddb3b730c8ab6016d54711df2806a6b5
wilker <plgu@hotmail.com>
2024-01-30 12:07:43 +0800

    WIP: intergrate reactjs

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
+
+import close from "../assets/close.svg";
+
+const Home = ({ home, provider, escrow, togglePop }) => {
+  return <div className="home"></div>;
+};
+
+export default Home;
